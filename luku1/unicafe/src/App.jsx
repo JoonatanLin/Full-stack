931477b7afad742eea6a86e7968ed5f536ebb09a
Joonatan Lindfors <joonatan.lindfors@gmail.com>
2026-02-09 14:18:45 +0200

    luku1: unicafe 1.6-1.11

---
+++ luku1/unicafe/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
+)
+
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+
+          <StatisticLine text="all" value={props.all} />
+          <StatisticLine text="average" value={props.average} />
+          <StatisticLine text="positive" value={props.positive + ' %'} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good * 1 + bad * -1) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  )
+}
+
+export default App
